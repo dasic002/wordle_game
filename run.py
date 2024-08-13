a4d5bc00a54f80d269f7b906dbc3bcc1cc0faea6
Carlos Silva <dasic002@yahoo.co.uk>
2024-08-13 15:18:24 +0000

    add basic guess input and check

--- run.py
+++ run.py
@@ -1,3 +1,16 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+# testing variable as the word of the day
+wod = "plate"
+
+def guess_input():
+    print("what is your guess?\n")
+    guess = input("your guess:")
+    if (guess.lower() == wod):
+        print(f"That's correct! {guess.upper()} is the word of the day")
+    else:
+        print(f"Oops! That guess is wrong. BYE!")
+
+guess_input()
