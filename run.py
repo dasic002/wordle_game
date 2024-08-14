63c0dd089d33cec9f075425e32f8eeb695e1027c
Carlos Silva <dasic002@yahoo.co.uk>
2024-08-14 07:36:59 +0000

    Add validation to check input is 5 chars long

--- run.py
+++ run.py
@@ -58,10 +58,33 @@ def guess_input(word):
     print(word)
     print("what is your guess?\n")
     guess = input("your guess:")
+
+    validate_input(guess.lower())
     if (guess.lower() == word):
         print(f"That's correct! {guess.upper()} is the word of the day")
     else:
         print(f"Oops! That guess is wrong. BYE!")
+
+
+def validate_input(value):
+    """
+    Validates input against our criteria:
+    - 5 characters long
+    - all characters are letter
+    - input is a word in our dictionary
+    """
+    try:
+        # Start - very similar to the love sandwiches validate data
+        if len(value) != 5:
+            raise ValueError(
+                f"The game only accepts 5 letter inputs, you provided {len(value)}"
+            )
+        # End - very similar to the love sandwiches validate data
+        
+    # Start - very similar to the love sandwiches validate data
+    except ValueError as e:
+        print(f"invalid data: {e}, please try again.\n")
+    # End - very similar to the love sandwiches validate data
 
 
 def main():
